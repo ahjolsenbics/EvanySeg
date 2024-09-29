efb6ba5b2d07c9305d0fc984782a6ce06bd368f4
ahjolsenbics <ahjol.senbi.cs@gmail.com>
2024-09-29 09:26:07 +0800

    9.29.1

--- test.py
+++ test.py
@@ -56,7 +56,7 @@ if __name__ == "__main__":
     parser.add_argument('--flag', type=int, default=1, help="1 for dice; 2 for dice and hd")
     parser.add_argument("--dataset_path", type=str, default='./datasets/preprocess/test_sam_Polyp', help='path to dataset')
     parser.add_argument("--result_path", type=str, default="./result")
-    parser.add_argument("--model_path", type=str, default='./result/ViT_base_bbox_dice.pth')
+    parser.add_argument("--model_path", type=str, default='./result/exp_1/checkpoints/ViT_base_bbox_dice.pth')
     parser.add_argument("--classification_model_name", type=str, default="vit_base_224", help='[ resnet50 | resnet101 | vit_base_224 | vit_large_224 ]')
     args = parser.parse_args()
 
